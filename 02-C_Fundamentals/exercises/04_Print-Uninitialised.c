02df5b3dba89c9a3afb5a4d8750af9fe3b634d04
twnc <17169285+twnc@users.noreply.github.com>
2021-02-06 22:20:02 +0000

    Added all remaining Chapter 2 Exercises (without solutions)

---
+++ 02-C_Fundamentals/exercises/04_Print-Uninitialised.c
@@ -0,0 +1,3 @@
+// Write a program that declares several int and float variables - without
+// initialising them - and then prints their values. Is there any pattern
+// to the values? (Usually there isn't.)
