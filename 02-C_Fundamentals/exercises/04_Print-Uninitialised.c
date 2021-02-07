69403c11c2f936558a01cc5eb025a61278b2eed5
twnc <17169285+twnc@users.noreply.github.com>
2021-02-07 14:28:41 +0000

    Ch02 Exercise 4 completed

--- 02-C_Fundamentals/exercises/04_Print-Uninitialised.c
+++ 02-C_Fundamentals/exercises/04_Print-Uninitialised.c
@@ -1,3 +1,25 @@
 // Write a program that declares several int and float variables - without
 // initialising them - and then prints their values. Is there any pattern
 // to the values? (Usually there isn't.)
+// ANSWER: All float values came back as 0.0000. int x was always a positive
+// 9 digit integer, int y was always 32766, int z was always a negative 9
+// digit integer. This was while compiled on MacOS, with both gcc and clang
+#include<stdio.h>
+
+int main(void)
+{
+	int x, y, z;
+	float a, b, c;
+
+	// Print uninitialised ints
+	printf("int x = %d\n", x);
+	printf("int y = %d\n", y);
+	printf("int z = %d\n", z);
+
+	// Print unitialised floats
+	printf("float a = %f\n", a);
+	printf("float b = %f\n", b);
+	printf("float c = %f\n", c);
+
+	return 0;
+}
