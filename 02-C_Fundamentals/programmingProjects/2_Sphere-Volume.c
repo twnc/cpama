c225d743cf4f1efa8bba69e38f5320d942d6a19f
twnc <17169285+twnc@users.noreply.github.com>
2021-02-07 19:46:13 +0000

    Ch02 Programming Project 2 completed

--- 02-C_Fundamentals/programmingProjects/2_Sphere-Volume.c
+++ 02-C_Fundamentals/programmingProjects/2_Sphere-Volume.c
@@ -2,3 +2,16 @@
 // using the formula v = 4/3πr³. Write the fraction 4/3 as 4.0f/3.0f. (Try 
 // writing it as 4/3. What happens?) Hint: C doesn't have an exponentiation
 // operator, so you'll need to multiply r by itself twice to compute r³.
+
+#include<stdio.h>
+#define PI (355.0f/113.0f)
+
+int main(void)
+{
+	float radius = 10.0f;
+	float volume = (4.0f/3.0f) * PI * radius * radius * radius;
+
+	printf("A sphere with a radius of %.2f has a volume of %.2f meters cubed.\n", radius, volume);
+
+	return 0;
+}
