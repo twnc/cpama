55524370f0e0c61bf6b554a9334aff69d941f628
twnc <17169285+twnc@users.noreply.github.com>
2021-02-08 21:42:09 +0000

    Ch02 Programming Project 6 - Added base code
    Question asks you to modify your answer to the previos question.
    The previous has been added (but not yet modified).

--- 02-C_Fundamentals/programmingProjects/6_Horners-Rule.c
+++ 02-C_Fundamentals/programmingProjects/6_Horners-Rule.c
@@ -3,3 +3,18 @@
 // ((((3x + 2)x - 5)x - 1)x + 7)x - 6
 // Note that the modified program performs fewer multiplications. This technique 
 // for evaluating polynoials is known as Homer's Rule.
+#include<stdio.h>
+
+int main(void)
+{
+	int x;
+	printf("Please enter a value for x: ");
+	scanf("%d", &x);
+
+	int answer = (3 * x * x * x * x * x) + (2 * x * x * x * x) -
+		     (5 * x * x * x) - (x * x) + (7 * x) - 6;
+
+	printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 = %d\n", answer);
+
+	return 0;
+}
