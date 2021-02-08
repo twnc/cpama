76df1e7619540c25e207605017bb47a6edeac4f7
twnc <17169285+twnc@users.noreply.github.com>
2021-02-08 21:47:27 +0000

    Ch02 Programming Project 6 completed

--- 02-C_Fundamentals/programmingProjects/6_Horners-Rule.c
+++ 02-C_Fundamentals/programmingProjects/6_Horners-Rule.c
@@ -11,8 +11,7 @@ int main(void)
 	printf("Please enter a value for x: ");
 	scanf("%d", &x);
 
-	int answer = (3 * x * x * x * x * x) + (2 * x * x * x * x) -
-		     (5 * x * x * x) - (x * x) + (7 * x) - 6;
+	int answer = ((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6;
 
 	printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 = %d\n", answer);
 
