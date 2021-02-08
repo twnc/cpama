14d653460e894f2f1f755b1b85b24619e251f9ea
twnc <17169285+twnc@users.noreply.github.com>
2021-02-08 20:54:31 +0000

    Ch02 Programming Project 4 completed

--- 02-C_Fundamentals/programmingProjects/4_Add-Tax.c
+++ 02-C_Fundamentals/programmingProjects/4_Add-Tax.c
@@ -2,3 +2,16 @@
 // displays the amount with 5% tax added:
 // Enter an amount: 100.00
 // With tax added: $105.00
+#include<stdio.h>
+
+int main(void)
+{
+	float money;
+	float taxRate = 1.05f;
+
+	printf("Enter an amount: ");
+	scanf("%f", &money);
+	printf("With tax added: $%.2f\n", money * taxRate);
+
+	return 0;
+}
