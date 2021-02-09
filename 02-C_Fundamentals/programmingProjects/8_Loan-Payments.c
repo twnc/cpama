ef8001af342f4ae3a45b0661b1726ca4c99d173a
twnc <17169285+twnc@users.noreply.github.com>
2021-02-09 14:21:43 +0000

    Ch02 Programming Project 8 completed

--- 02-C_Fundamentals/programmingProjects/8_Loan-Payments.c
+++ 02-C_Fundamentals/programmingProjects/8_Loan-Payments.c
@@ -13,3 +13,30 @@
 // the balance is decreased by the amount of the payment, but increased by the 
 // balance times the monthly interest rate. To find the monthly interest rate, 
 // convert the interest rate entered by the user to a percentage and divide it by 12.
+
+#include<stdio.h>
+
+int main(void)
+{
+	float loan, annual_interest_rate, monthly_payment, monthly_interest_rate;
+	printf("Enter amount of loan: ");
+	scanf("%f", &loan);
+	printf("Enter interest rate: ");
+	scanf("%f", &annual_interest_rate);
+	printf("Enter monthly payment: ");
+	scanf("%f", &monthly_payment);
+	printf("\n");
+
+	monthly_interest_rate = 1.0f + (annual_interest_rate / 12.0f / 100.0f);
+	annual_interest_rate = 1.0f + (annual_interest_rate / 100.0f);
+
+	loan = loan * monthly_interest_rate - monthly_payment;
+	printf("Balance reamining after first payment: $%.2f\n", loan);
+	loan = loan * monthly_interest_rate - monthly_payment;
+	printf("Balance reamining after second payment: $%.2f\n", loan);
+	loan = loan * monthly_interest_rate - monthly_payment;
+	printf("Balance reamining after third payment: $%.2f\n", loan);
+
+
+	return 0;
+}
