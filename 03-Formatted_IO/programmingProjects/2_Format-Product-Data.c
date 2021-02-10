71df56658794194e0b7d507bc827f99bd198e3a6
twnc <17169285+twnc@users.noreply.github.com>
2021-02-10 22:06:43 +0000

    Ch03 Programming Project 2 completed

--- 03-Formatted_IO/programmingProjects/2_Format-Product-Data.c
+++ 03-Formatted_IO/programmingProjects/2_Format-Product-Data.c
@@ -11,3 +11,29 @@
 //
 // The item number and date should be left justified; the unit price should be right
 // justified. Allow dollar amounts up to $9999.99. Hint: Use tabs to line up the columns.
+#include<stdio.h>
+
+int main(void)
+{
+	int item_number, purchase_month, purchase_day, purchase_year;
+	float unit_price;
+
+	printf("Enter item number: ");
+	scanf("%d", &item_number);
+
+	printf("Enter unit price: ");
+	scanf("%f", &unit_price);
+	if(unit_price > 9999.99) unit_price = 9999.99;
+
+	printf("Enter purchase date (mm/dd/yyyy) : ");
+	scanf("%d /%d /%d", &purchase_month, &purchase_day, &purchase_year);
+	printf("\n");
+
+	printf("Item\t\tUnit\t\tPurchase\n");
+	printf("\t\tPrice\t\tDate\n");
+	printf("%d\t\t", item_number);
+	printf("$%7.2f\t", unit_price);
+	printf("%d/%d/%d\n", purchase_month, purchase_day, purchase_year);
+
+	return 0;
+}
