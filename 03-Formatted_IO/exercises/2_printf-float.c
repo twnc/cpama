6ee8b989a671b5cb76b22fbc75f72231985b4e94
twnc <17169285+twnc@users.noreply.github.com>
2021-02-10 16:41:10 +0000

    Ch03 Exercise 2 completed

--- 03-Formatted_IO/exercises/2_printf-float.c
+++ 03-Formatted_IO/exercises/2_printf-float.c
@@ -8,16 +8,16 @@ int main(void)
 
 	// (a) Exponential notation; left justified in a field of size 8;
 	//     one digit after the decimal point.
-	printf("(a) = %f\n", x);
+	printf("(a) = %8.1e\n", x);
 	// (b) Exponential notation; right-justified in a field of size 10;
 	//     six digits after the decimal point.
-	printf("(b) = %f\n", x);
+	printf("(b) = %-10.6e\n", x);
 	// (c) Fixed decimal notation; left-justified in a field of size 8;
 	//     three digits after the decimal point.
-	printf("(c) = %f\n", x);
+	printf("(c) = %8.3f\n", x);
 	// (d) Fixed decimal notation; right-justified in a field of size 6;
 	//     no digits after the decimal point.
-	printf("(d) = %f\n", x);
+	printf("(d) = %-6.0f\n", x);
 
 	return 0;
 }
