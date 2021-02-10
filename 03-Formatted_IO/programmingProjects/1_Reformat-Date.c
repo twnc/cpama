7fa82ee1880b4be41c2ed2025546c97015838426
twnc <17169285+twnc@users.noreply.github.com>
2021-02-10 20:05:09 +0000

    Ch03 Programming Project completed

--- 03-Formatted_IO/programmingProjects/1_Reformat-Date.c
+++ 03-Formatted_IO/programmingProjects/1_Reformat-Date.c
@@ -3,3 +3,17 @@
 //
 // Enter a date (mm/dd/yyyy) : 2/17/2011
 // You entered the date 20110217
+
+#include<stdio.h>
+
+int main(void)
+{
+	int day, month, year;
+	
+	printf("Enter a date (mm/dd/yyyy) : ");
+	scanf("%d /%d /%d", &month, &day, &year);
+
+	printf("You entered the date %d%.2d%.2d\n", year, month, day);
+
+	return 0;
+}
