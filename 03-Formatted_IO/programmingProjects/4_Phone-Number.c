ced163fc09828a3ef7e8787d6e3e6561aff3f4f2
twnc <17169285+twnc@users.noreply.github.com>
2021-02-10 22:24:06 +0000

    Ch03 Programming Project 4 completed

--- 03-Formatted_IO/programmingProjects/4_Phone-Number.c
+++ 03-Formatted_IO/programmingProjects/4_Phone-Number.c
@@ -1,4 +1,17 @@
-// Write a program that prompts the user to enter a telephone number in the form (xxx) xxx-xxxx and then displays the number in the form xxx.xxx.xxxx:
+// Write a program that prompts the user to enter a telephone number in the form
+// (xxx) xxx-xxxx and then displays the number in the form xxx.xxx.xxxx:
 //
 // Enter phone number [(xxx) xxx-xxxx] : (404) 817-6900
 // You entered 404.817.6900
+#include<stdio.h>
+
+int main(void)
+{
+	int num_part1, num_part2, num_part3;
+	printf("Enter phone number [(xxx) xxx-xxxx] : ");
+	scanf(" (%d )%d -%d", &num_part1, &num_part2, &num_part3);
+
+	printf("You entered %d.%d.%d\n", num_part1, num_part2, num_part3);
+
+	return 0;
+}
