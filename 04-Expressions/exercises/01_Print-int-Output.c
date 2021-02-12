a953c32f0183321459ad2957a0a9ded2fde52b11
twnc <17169285+twnc@users.noreply.github.com>
2021-02-12 20:47:24 +0000

    Ch04 Exercise 1 added (uncompleted)
    Exercise asks you to guess the output of program fragments. I've
    put the fragments inside a program so that I can test my answers
    afterwards.

---
+++ 04-Expressions/exercises/01_Print-int-Output.c
@@ -0,0 +1,26 @@
+// Show the output produced by each of the following program fragments. Assume that i, j, and k are int variables.
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+
+	// (a) 
+	// i = 5; j = 3;
+	// printf("%d %d\n", i / j, i % j);
+	
+	// (b) 
+	// i = 2; j = 3;
+	// printf("%d\n", (i + 10) % j);
+	
+	// (c)
+	// i = 7; j = 8; k = 9;
+	// printf("%d\n", (i + 10) % k / j);
+	
+	// (d)
+	// i = 1; j = 2; k = 3;
+	// printf("%d\n", (i + 5) % (j + 2) / k);
+
+	return 0;
+}
