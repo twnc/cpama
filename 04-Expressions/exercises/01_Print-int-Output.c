0845b6f8c470486d00a409891964fc554fb22144
twnc <17169285+twnc@users.noreply.github.com>
2021-02-20 21:58:43 +0000

    Ch04 Exercise 1 completed and answers tested

--- 04-Expressions/exercises/01_Print-int-Output.c
+++ 04-Expressions/exercises/01_Print-int-Output.c
@@ -7,20 +7,24 @@ int main(void)
 	int i, j, k;
 
 	// (a) 
-	// i = 5; j = 3;
-	// printf("%d %d\n", i / j, i % j);
+	i = 5; j = 3;
+	printf("%d %d\n", i / j, i % j);
+	// Answer: 1 2
 	
 	// (b) 
-	// i = 2; j = 3;
-	// printf("%d\n", (i + 10) % j);
+	i = 2; j = 3;
+	printf("%d\n", (i + 10) % j);
+	// Answer: 0
 	
 	// (c)
-	// i = 7; j = 8; k = 9;
-	// printf("%d\n", (i + 10) % k / j);
+	i = 7; j = 8; k = 9;
+	printf("%d\n", (i + 10) % k / j);
+	// Answer: 1
 	
 	// (d)
-	// i = 1; j = 2; k = 3;
-	// printf("%d\n", (i + 5) % (j + 2) / k);
+	i = 1; j = 2; k = 3;
+	printf("%d\n", (i + 5) % (j + 2) / k);
+	// Answer: 0
 
 	return 0;
 }
