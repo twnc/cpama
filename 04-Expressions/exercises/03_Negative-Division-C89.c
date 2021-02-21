0f8a09c95001a984738c57549516e3f62c949c1b
twnc <17169285+twnc@users.noreply.github.com>
2021-02-21 16:00:26 +0000

    Ch04 Exercise 3 comment style modified for C89 compatibility

--- 04-Expressions/exercises/03_Negative-Division-C89.c
+++ 04-Expressions/exercises/03_Negative-Division-C89.c
@@ -1,23 +1,25 @@
-// What is the value of each of the following expressions in C89? (Give all
-// possible values if an expression may have more than one value.)
+/*
+ * What is the value of each of the following expressions in C89? (Give all
+ * possible values if an expression may have more than one value.)
+ */
 #include<stdio.h>
 
 int main(void) {
-	// (a) 8 / 5
+	/* (a) 8 / 5 */
 	printf("%d\n", 8 / 5);
-	// Answer: 1
+	/* Answer: 1 */
 
-	// (b) -8 / 5
+	/* (b) -8 / 5 */
 	printf("%d\n", -8 / 5);
-	// Answer: -1 or -2 (implementation dependent)
+	/* Answer: -1 or -2 (implementation dependent) */
 
-	// (c) 8 / -5
+	/* (c) 8 / -5 */
 	printf("%d\n", 8 / -5);
-	// Answer: -1 or -2 (implementation dependent)
+	/* Answer: -1 or -2 (implementation dependent) */
 
-	// (d) -8 / -5
+	/* (d) -8 / -5 */
 	printf("%d\n", -8 / -5);
-	// Answer: 1 or 2 (implementation dependent)
+	/* Answer: 1 or 2 (implementation dependent) */
 
 	return 0;
 }
