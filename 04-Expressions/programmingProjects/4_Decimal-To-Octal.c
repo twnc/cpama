0c5681b5b00f82bc01eadbc46258ef3d34577942
twnc <17169285+twnc@users.noreply.github.com>
2021-02-22 20:11:56 +0000

    Ch04 Programming Project 4 added (uncompleted without hint)

---
+++ 04-Expressions/programmingProjects/4_Decimal-To-Octal.c
@@ -0,0 +1,8 @@
+// Write a program that reads an integer entered by the user and displays it in
+// octal (base 8):
+//
+// Enter a number between 0 and 32767: 1953
+// In octal, your number is: 03641
+//
+// The output should be displayed using five digits, even if few digits are
+// sufficient. Hint: ADD LATER
