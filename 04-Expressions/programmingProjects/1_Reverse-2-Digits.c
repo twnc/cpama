95b7f42092f236af49c9bcbe21fd35745426101f
twnc <17169285+twnc@users.noreply.github.com>
2021-02-22 19:52:22 +0000

    Ch04 Programming Project 1 completed

--- 04-Expressions/programmingProjects/1_Reverse-2-Digits.c
+++ 04-Expressions/programmingProjects/1_Reverse-2-Digits.c
@@ -8,3 +8,16 @@
 // Read the number using %d, then break it into two digits. Hint: If n is an
 // integer, then n % 10 is the last digit in n and n / 10 is n with the last
 // digit removed.
+
+#include<stdio.h>
+
+int main(void)
+{
+	int number;
+
+	printf("Enter a two-digit number: ");
+	scanf("%2d", &number);
+	printf("The reversal is: %d%d\n", number%10, number/10);
+
+	return 0;
+}
