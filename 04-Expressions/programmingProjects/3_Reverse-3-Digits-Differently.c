601ec11a89c75184149e5feed9e092c06e1071a4
twnc <17169285+twnc@users.noreply.github.com>
2021-02-22 20:03:20 +0000

    Ch04 Programming Project 3 completed

--- 04-Expressions/programmingProjects/3_Reverse-3-Digits-Differently.c
+++ 04-Expressions/programmingProjects/3_Reverse-3-Digits-Differently.c
@@ -1,3 +1,16 @@
 // Rewrite the program in Programming Project 2 so that it prints the reversal
 // of a three-digit number without using arithmetic to split the number into
 // digits. Hint: See the puc.c program of section 4.1.
+
+#include<stdio.h>
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	printf("Enter a three-digit number: ");
+	scanf("%1d%1d%1d", &num3, &num2, &num1);
+	printf("The reversal is: %d%d%d\n", num1, num2, num3);
+
+	return 0;
+}
