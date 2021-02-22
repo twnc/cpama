bfc585fcfbb4d76404907af730b221e5b4db0919
twnc <17169285+twnc@users.noreply.github.com>
2021-02-22 19:59:30 +0000

    Ch04 Programming Project 2 completed

--- 04-Expressions/programmingProjects/2_Reverse-3-Digits.c
+++ 04-Expressions/programmingProjects/2_Reverse-3-Digits.c
@@ -1,1 +1,14 @@
 // Extend the program in Programming Project 1 to handle three-digit numbers.
+
+#include<stdio.h>
+
+int main(void)
+{
+	int number;
+
+	printf("Enter a three-digit number: ");
+	scanf("%3d", &number);
+	printf("The reversal is: %d%d%d\n", number%10, number/10%10, number/100%10);
+
+	return 0;
+}
