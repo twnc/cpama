57bb9c67becb6188cdc3c875c58739338d1f36b7
twnc <17169285+twnc@users.noreply.github.com>
2021-02-22 19:46:58 +0000

    Ch04 Programming Project 4 added (uncompleted)

---
+++ 04-Expressions/programmingProjects/3_Reverse-3-Digits-Differently.c
@@ -0,0 +1,3 @@
+// Rewrite the program in Programming Project 2 so that it prints the reversal
+// of a three-digit number without using arithmetic to split the number into
+// digits. Hint: See the puc.c program of section 4.1.
