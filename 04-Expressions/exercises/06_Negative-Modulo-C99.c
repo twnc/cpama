3ba705e6911b0370991b4420d1a22da310268f02
twnc <17169285+twnc@users.noreply.github.com>
2021-02-23 22:17:18 +0000

    Ch04 Exercise 6 added (uncompleted)

---
+++ 04-Expressions/exercises/06_Negative-Modulo-C99.c
@@ -0,0 +1,21 @@
+ // What is the value of each of the following expressions in C99? (Give all
+ // possible values if an expression may have more than one value.)
+
+#include<stdio.h>
+
+int main(void)
+{
+	// (a)  8 %  5
+	printf(" 8 %%  5 = %2d\n", 8 % 5);
+
+	// (b) -8 %  5
+	printf("-8 %%  5 = %2d\n", -8 % 5);
+
+	// (c)  8 % -5
+	printf(" 8 %% -5 = %2d\n", 8 % -5);
+
+	// (d) -8 % -5
+	printf("-8 %% -5 = %2d\n", -8 % -5);
+
+	return 0;
+}
