59084436df53470bf2855a5c42f8a045ac7c546f
twnc <17169285+twnc@users.noreply.github.com>
2021-02-25 13:41:42 +0000

    Ch04 Exercise 15 added (uncompleted)

---
+++ 04-Expressions/exercises/15_Example-Output.c
@@ -0,0 +1,8 @@
+// Give the values of i and j after each of the following expression statements
+// has been executed. (Assume that i has the value 1 initially and j has the
+// value 2.)
+//
+// (a) i += j;
+// (b) i --;
+// (c) i * j / i;
+// (d) i % ++j;
