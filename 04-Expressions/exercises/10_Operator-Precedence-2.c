36ec9d4307e293f3dab15e3e23cefce9781465f0
twnc <17169285+twnc@users.noreply.github.com>
2021-02-26 18:25:12 +0000

    Ch04 Exercise 10 completed

--- 04-Expressions/exercises/10_Operator-Precedence-2.c
+++ 04-Expressions/exercises/10_Operator-Precedence-2.c
@@ -11,21 +11,25 @@ int main(void)
 	i = 6;
 	j = i += i;
 	printf("%d %d\n", i, j);
+	// Answer: 12 12
 
 	// (b)
 	i = 5;
 	j = (i -= 2) + 1;
 	printf("%d %d\n", i, j);
+	// Answer: 3 4
 
 	// (c)
 	i = 7;
 	j = 6 + (i = 2.5);
 	printf("%d %d\n", i, j);
+	// Answer: 2 8
 
 	// (d)
 	i = 2; j = 8;
 	j = (i = 6) + (j = 3);
 	printf("%d %d\n", i, j);
+	// Answer: 6 9
 
 	return 0;
 }
