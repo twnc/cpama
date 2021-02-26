10ab9a6a10ddb74be0bdbb5b7530cacf60030bda
twnc <17169285+twnc@users.noreply.github.com>
2021-02-26 20:48:16 +0000

    Ch04 Programming Project 6 completed

--- 04-Expressions/programmingProjects/6_EAN.c
+++ 04-Expressions/programmingProjects/6_EAN.c
@@ -24,17 +24,14 @@
 
 int main(void)
 {
-	int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, first_sum, second_sum, total;
+	int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, first_sum, second_sum, total;
 
-	printf("Enter the first (single) digit: ");
-	scanf("%1d", &d);
-	printf("Enter first group of five digits: ");
-	scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
-	printf("Enter second group of five digits: ");
-	scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
+	printf("Enter the first 12 digits of an EAN: ");
+	scanf("%1d%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5, &i6);
+	scanf("%1d%1d%1d%1d%1d%1d", &i7, &i8, &i9, &i10, &i11, &i12);
 
-	first_sum = d + i2 + i4 + j1 + j3 + j5;
-	second_sum = i1 + i3 + i5 + j2 + j4;
+	first_sum = i2 + i4 + i6 + i8 + i10 + i12;
+	second_sum = i1 + i3 + i5 + i7 + i9 + i11;
 	total = 3 * first_sum + second_sum;
 
 	printf("Check digit: %d\n", 9 - ((total - 1) % 10));
