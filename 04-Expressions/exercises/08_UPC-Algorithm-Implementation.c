a519f98bd56d4acc339162958ca2b43121d49c24
twnc <17169285+twnc@users.noreply.github.com>
2021-02-26 17:37:05 +0000

    Ch04 Exercise 8 completed

--- 04-Expressions/exercises/08_UPC-Algorithm-Implementation.c
+++ 04-Expressions/exercises/08_UPC-Algorithm-Implementation.c
@@ -1,2 +1,6 @@
 // Would the upc.c program still work if the expression 9 - ((total - 1) % 10)
 // were replaced by (10 - (total % 10)) % 10?
+
+// Answer: No, however the output would be the same most of the time. It would
+// differ however, whenever total was a multiple of 10. Then the former would
+// always give 0 as a result, whereas the replacement would give 10 as a result.
