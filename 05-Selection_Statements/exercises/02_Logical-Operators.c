9035efa1da3cd0f0ee0dfcb6cc9a05633c64bd54
twnc <17169285+twnc@users.noreply.github.com>
2021-02-27 20:29:44 +0000

    Ch05 Exercise 2 added (uncompleted)

---
+++ 05-Selection_Statements/exercises/02_Logical-Operators.c
@@ -0,0 +1,27 @@
+// The following program fragments illustrate the logical operators. Show the
+// output produced by each, assuming that i, j and k are int variables.
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+
+	// (a)
+	i = 10; j = 5;
+	printf("%d\n", !i < j);
+
+	// (b)
+	i = 2; j = 1;
+	printf("%d\n", !!i + !j);
+
+	// (c)
+	i = 5; j = 0; k = -5;
+	printf("%d\n", i && j || k);
+
+	// (d)
+	i = 1; j = 2; k = 3;
+	printf("%d\n", i < j || k);
+
+	return 0;
+}
