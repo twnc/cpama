2aa652c5053270e9ca32ae988710052a047e42fd
twnc <17169285+twnc@users.noreply.github.com>
2021-02-28 16:59:38 +0000

    Ch05 Exercise 7 added (uncompleted)

---
+++ 05-Selection_Statements/exercises/07_Conditional-Expression.c
@@ -0,0 +1,15 @@
+// What does the following statement print if i has the value 17? What it print
+// if i has the value -17?
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i = 17;
+	printf("%d\n", i >= 0 ? i : -i);
+
+	i = -17;
+	printf("%d\n", i >= 0 ? i : -i);
+
+	return 0;
+}
