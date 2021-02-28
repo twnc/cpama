e3ada005e24df53b7820a94e45d4b236a3eb0ed6
twnc <17169285+twnc@users.noreply.github.com>
2021-02-28 15:46:30 +0000

    Ch05 Exercise 5 added (uncompleted)

---
+++ 05-Selection_Statements/exercises/05_If-Statement-Legality-1.c
@@ -0,0 +1,14 @@
+// Is the following if statement legal?
+// If so, what does it do when n is equal to 0?
+
+#include<stdio.h>
+
+int main(void)
+{
+	int n = 0;
+
+	if (n >= 1 <= 10)
+		printf("n is between 1 and 10\n");
+
+	return 0;
+}
