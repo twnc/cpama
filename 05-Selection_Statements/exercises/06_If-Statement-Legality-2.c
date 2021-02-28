def03642fd4ac1dc0a5ce821110d6ee8ca0c3d31
twnc <17169285+twnc@users.noreply.github.com>
2021-02-28 15:47:06 +0000

    Ch05 Exercise 6 added (uncompleted)

---
+++ 05-Selection_Statements/exercises/06_If-Statement-Legality-2.c
@@ -0,0 +1,14 @@
+// Is the following if statement legal?
+// If so, what does it do when n is equal to 5?
+
+#include<stdio.h>
+
+int main(void)
+{
+	int n = 5;
+
+	if (n == 1-10)
+		printf("n is between 1 and 10\n");
+
+	return 0;
+}
