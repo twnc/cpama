f1319f98422969e5c00faaad9b6604988ea8bacd
twnc <17169285+twnc@users.noreply.github.com>
2021-03-01 14:30:22 +0000

    Ch05 Exercise 11 added (uncompleted)

---
+++ 05-Selection_Statements/exercises/11_Phone-Code-Switch.c
@@ -0,0 +1,19 @@
+// The following table shows telephone area codes in the state of Georgia along
+// with the largest city in each area:
+//
+// Area code	Major City
+//    229	Albany
+//    404	Atlanta
+//    470	Atlanta
+//    478	Macon
+//    678	Atlanta
+//    706	Columbus
+//    762	Columbus
+//    770	Atlanta
+//    912	Savannah
+//
+// Write a switch statement whose controlling expression is the variable
+// area_code. If the value of area_code is in the table, the switch statement
+// will print the corresponding city name. Otherwise, the switch statement will
+// display the message "Area code not recognised". Use the techniques discussed
+// in Section 5.3 to make the switch statement as simple as possible.
