068cef40d52a3fbfad6d7b0973d3c04afc383722
twnc <17169285+twnc@users.noreply.github.com>
2021-03-02 21:33:40 +0000

    Ch05 Exercise 5 completed

--- 05-Selection_Statements/exercises/05_If-Statement-Legality-1.c
+++ 05-Selection_Statements/exercises/05_If-Statement-Legality-1.c
@@ -1,5 +1,12 @@
 // Is the following if statement legal?
 // If so, what does it do when n is equal to 0?
+//
+// Answer: Yes it is legal. The >= and <= operators have equal precedence and
+// have left associativity. This means that the if condition will be evaluated
+// as if it were if ((n >= 1) <= 10). Whatever the value of n, the statement
+// will always evaluate to true. If (n >= 1) is true, it will evaluate to 1,
+// otherwise it will evaluate to 0. Both of these numbers are less than 10, so
+// in this case the if statement will always be true.
 
 #include<stdio.h>
 
