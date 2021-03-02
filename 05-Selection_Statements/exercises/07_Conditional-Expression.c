2cda5fd75c701b64d08c2e5bf8ba0b8e805ed226
twnc <17169285+twnc@users.noreply.github.com>
2021-03-02 21:42:04 +0000

    Ch05 Exercise 7 completed

--- 05-Selection_Statements/exercises/07_Conditional-Expression.c
+++ 05-Selection_Statements/exercises/07_Conditional-Expression.c
@@ -1,5 +1,8 @@
 // What does the following statement print if i has the value 17? What does it
 // print if i has the value -17?
+// Answer: This statement will return all positive numbers as they were. If a
+// number is negative, it will return it as a positive number (by double
+// negating it). So both 17 and -17 will be returned as 17.
 
 #include<stdio.h>
 
