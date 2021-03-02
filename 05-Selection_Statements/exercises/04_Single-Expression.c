79c259d187a7e17e7bc2f7ceae7a13efcd6999f1
twnc <17169285+twnc@users.noreply.github.com>
2021-03-02 21:19:45 +0000

    Ch05 Exercise 4 completed

--- 05-Selection_Statements/exercises/04_Single-Expression.c
+++ 05-Selection_Statements/exercises/04_Single-Expression.c
@@ -1,2 +1,19 @@
 // Write a single expression whose value is either -1, 0, or +1, depending on
 // whether i is less than, equal to, or greater than j, respectively.
+// Answer: i < j ? -1 : i == j ? 0 : 1
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	i = 7; j = 10;
+	printf("%d\n", i < j ? -1 : i == j ? 0 : 1);
+	i = 7; j = 7;
+	printf("%d\n", i < j ? -1 : i == j ? 0 : 1);
+	i = 10; j = 7;
+	printf("%d\n", i < j ? -1 : i == j ? 0 : 1);
+
+	return 0;
+}
