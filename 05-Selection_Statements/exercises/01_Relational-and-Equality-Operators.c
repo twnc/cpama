f661387e9625668d77c8b8e22d26270466a658c8
twnc <17169285+twnc@users.noreply.github.com>
2021-03-02 11:58:31 +0000

    Ch05 Exercise 1 completed

--- 05-Selection_Statements/exercises/01_Relational-and-Equality-Operators.c
+++ 05-Selection_Statements/exercises/01_Relational-and-Equality-Operators.c
@@ -12,18 +12,22 @@ int main(void)
 	i = 2; j = 3;
 	k = i * j == 6;
 	printf("%d\n", k);
+	// Answer: 1
 
 	// (b)
 	i = 5; j = 10; k = 1;
 	printf("%d\n", k > i < j);
+	// Answer: 1
 
 	// (c)
 	i = 3; j = 2; k = 1;
 	printf("%d\n", i < j == j < k);
+	// Answer: 1
 
 	// (d)
 	i = 3; j = 4; k = 5;
 	printf("%d\n", i % j + i < k);
+	// Answer: 0
 
 	return 0;
 }
