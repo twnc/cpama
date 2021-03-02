13d91bbb5f8e280ca9107c60b2e4ec90521ae7dd
twnc <17169285+twnc@users.noreply.github.com>
2021-03-02 21:36:59 +0000

    Ch05 Exercise 6 completed

--- 05-Selection_Statements/exercises/06_If-Statement-Legality-2.c
+++ 05-Selection_Statements/exercises/06_If-Statement-Legality-2.c
@@ -1,5 +1,10 @@
 // Is the following if statement legal?
+// Answer: Yes
 // If so, what does it do when n is equal to 5?
+// Answer: It looks at a glance like it might calculate a range, and see if n is
+// between 1 and 10. However it will instead evaluate 1-10 as a subtraction
+// resulting in -9. As 5 does not equal -9, it will evaluate as false and
+// the printf function won't be carried out.
 
 #include<stdio.h>
 
