5536c41f51d90a33db01fab4ef5580171e775be9
twnc <17169285+twnc@users.noreply.github.com>
2021-03-03 10:00:45 +0000

    Ch05 Exercise 11 completed

--- 05-Selection_Statements/exercises/11_Phone-Code-Switch.c
+++ 05-Selection_Statements/exercises/11_Phone-Code-Switch.c
@@ -17,3 +17,30 @@
 // will print the corresponding city name. Otherwise, the switch statement will
 // display the message "Area code not recognised". Use the techniques discussed
 // in Section 5.3 to make the switch statement as simple as possible.
+
+#include<stdio.h>
+
+int main(void)
+{
+	int area_code;
+
+	printf("Please enter the area code: ");
+	scanf("%d", &area_code);
+
+	switch (area_code) {
+		case 229:
+			printf("Albany\n");			break;
+		case 404: case 470: case 678: case 770:
+			printf("Atlanta\n"); 			break;
+		case 706: case 762:
+			printf("Columbus\n");			break;
+		case 478:
+			printf("Macon\n");			break;
+		case 912:
+			printf("Savannah\n");			break;
+		default:
+			printf("Area code not recognised\n");	break;
+	}
+
+	return 0;
+}
