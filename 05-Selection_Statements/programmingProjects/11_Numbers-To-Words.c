59a6fcb52f6fdf8cc7204af8a92a4c83933f9035
twnc <17169285+twnc@users.noreply.github.com>
2021-03-05 17:56:55 +0000

    Ch05 Programming Project 11 completed

--- 05-Selection_Statements/programmingProjects/11_Numbers-To-Words.c
+++ 05-Selection_Statements/programmingProjects/11_Numbers-To-Words.c
@@ -8,3 +8,88 @@
 // word for the first digit ("twenty", "thirty", and so forth). Use a second
 // switch statement to print the word for the second digit. Don't forget that
 // the numbers between 11 and 19 require special treatment.
+
+#include<stdio.h>
+
+int main(void)
+{
+	int digit1, digit2;
+
+	printf("Enter a two-digit number: ");
+	scanf("%1d%1d", &digit1, &digit2);
+
+	printf("You entered the number ");
+
+	switch(digit1) {
+		case 9:
+			printf("ninety"); break;
+		case 8:
+			printf("eighty"); break;
+		case 7:
+			printf("seventy"); break;
+		case 6:
+			printf("sixty"); break;
+		case 5:
+			printf("fifty"); break;
+		case 4:
+			printf("forty"); break;
+		case 3:
+			printf("thirty"); break;
+		case 2:
+			printf("twenty"); break;
+	}
+
+	if(digit1 >= 2 && digit2 != 0)
+		printf("-");
+
+	if(digit1 == 1)
+		switch(digit2) {
+			case 9:
+				printf("nineteen"); break;
+			case 8:
+				printf("eighteen"); break;
+			case 7:
+				printf("seventeen"); break;
+			case 6:
+				printf("sixteen"); break;
+			case 5:
+				printf("fifteen"); break;
+			case 4:
+				printf("fourteen"); break;
+			case 3:
+				printf("thirteen"); break;
+			case 2:
+				printf("twelve"); break;
+			case 1:
+				printf("eleven"); break;
+			case 0:
+				printf("ten"); break;
+		}
+	else
+		switch(digit2) {
+			case 9:
+				printf("nine"); break;
+			case 8:
+				printf("eight"); break;
+			case 7:
+				printf("seven"); break;
+			case 6:
+				printf("six"); break;
+			case 5:
+				printf("five"); break;
+			case 4:
+				printf("four"); break;
+			case 3:
+				printf("three"); break;
+			case 2:
+				printf("two"); break;
+			case 1:
+				printf("one"); break;
+			case 0:
+				if(digit1 == 0) printf("zero"); break;
+		}
+
+	printf(".\n");
+
+	return 0;
+}
