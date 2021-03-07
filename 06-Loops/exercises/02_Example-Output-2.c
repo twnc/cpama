4d3bdeb0f98437c7b6d7d8e2d8e7370d81b8decb
twnc <17169285+twnc@users.noreply.github.com>
2021-03-07 21:29:16 +0000

    Ch06 Exercise 2 Added

---
+++ 06-Loops/exercises/02_Example-Output-2.c
@@ -0,0 +1,14 @@
+// What output does the following program produce?
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i = 9384;
+	do {
+		printf("%d ", i);
+		i /= 10;
+	} while (i > 0);
+
+	return 0;
+}
