ed4b21f19a29c13e7f4b9b3082814e8e2acea754
twnc <twnc@users.noreply.github.com>
2021-03-09 20:34:20 +0000

    Ch06 Exercise 10 completed

--- 06-Loops/exercises/10_Continue-to-goto.c
+++ 06-Loops/exercises/10_Continue-to-goto.c
@@ -7,9 +7,9 @@ int main(void)
 	int i = 0;
 	// Print even numbers up to 10;
 	while(i < 10) {
-		++i;
+loop_beginning: ++i;
 		if(i%2 != 0)
-			continue;
+			goto loop_beginning;
 		printf("%d\n", i);
 	}
 
