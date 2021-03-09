064d22b01c495138c3ea972fff94a3f49ab117c4
twnc <17169285+twnc@users.noreply.github.com>
2021-03-09 21:33:48 +0000

    Ch06 Programming Project 1 added

---
+++ 06-Loops/programmingProjects/01_Largest-Number.c
@@ -0,0 +1,15 @@
+// Write a program that finds the largest in a series of numbers entered by the
+// user. The program must prompt the user to enter numbers one by one. When the
+// user enter 0 or a negative number, the program must display the largest
+// nonnegative number entered:
+//
+// Enter a number: 60
+// Enter a number: 38.3
+// Enter a number: 4.89
+// Enter a number: 100.62
+// Enter a number: 75.2295
+// Enter a number: 0
+//
+// The largest number entered was 100.62
+//
+// Notice that the numbers aren't necessarily integers.
