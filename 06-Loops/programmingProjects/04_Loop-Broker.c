2b6eeecee314fd97e0873fe8c6d26917d6163e84
twnc <17169285+twnc@users.noreply.github.com>
2021-03-11 11:39:04 +0000

    Ch06 Programming Project 4 completed

--- 06-Loops/programmingProjects/04_Loop-Broker.c
+++ 06-Loops/programmingProjects/04_Loop-Broker.c
@@ -13,31 +13,36 @@
 // ORIGNAL: Calculates a broker's commission
 
 #include<stdio.h>
+#include<stdbool.h>
 
 int main(void)
 {
 	float commission, value;
 
-	printf("Enter value of trade: ");
-	scanf("%f", &value);
+	while (true) {
+		printf("Enter value of trade: ");
+		scanf("%f", &value);
+		if (value == 0)
+			break;
 
-	if(value < 2500.00f)
-		commission = 30.00f + value * .017f;
-	else if (value < 6250.00f)
-		commission = 56.00f + value * .0066f;
-	else if (value < 20000.00f)
-		commission = 76.00f + value * .0034f;
-	else if (value < 50000.00f)
-		commission = 100.00f + value * .0022f;
-	else if (value < 500000.00f)
-		commission = 155.00f + value * .0011f;
-	else
-		commission = 255.00f + value * .0009f;
+		if (value < 2500.00f)
+			commission = 30.00f + value * .017f;
+		else if (value < 6250.00f)
+			commission = 56.00f + value * .0066f;
+		else if (value < 20000.00f)
+			commission = 76.00f + value * .0034f;
+		else if (value < 50000.00f)
+			commission = 100.00f + value * .0022f;
+		else if (value < 500000.00f)
+			commission = 155.00f + value * .0011f;
+		else
+			commission = 255.00f + value * .0009f;
 
-	if (commission < 39.00f)
-		commission = 39.00f;
+		if (commission < 39.00f)
+			commission = 39.00f;
 
-	printf("Commission: $%.2f\n", commission);
+		printf("Commission: $%.2f\n\n", commission);
+	}
 
 	return 0;
 }
