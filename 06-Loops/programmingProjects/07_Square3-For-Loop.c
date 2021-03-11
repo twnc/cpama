22323ca360daf75edfb3315340ae1846c0f48fe2
twnc <17169285+twnc@users.noreply.github.com>
2021-03-11 12:47:16 +0000

    Ch06 Programming Project 7 completed

--- 06-Loops/programmingProjects/07_Square3-For-Loop.c
+++ 06-Loops/programmingProjects/07_Square3-For-Loop.c
@@ -13,11 +13,9 @@ int main(void)
 	printf("Enter number of entries in table: ");
 	scanf("%d", &n);
 
-	i = 1;
 	odd = 3;
-	for (square = 1; i <= n; odd += 2) {
+	for (square = 1, i = 1; i <= n; odd += 2, ++i) {
 		printf("%10d%10d\n", i, square);
-		++i;
 		square += odd;
 	}
 
