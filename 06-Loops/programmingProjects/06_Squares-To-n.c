05cc4857090991b634dfbc9e0e8e52af86319d79
twnc <17169285+twnc@users.noreply.github.com>
2021-03-11 12:16:19 +0000

    Ch06 Programming Project 6 added

---
+++ 06-Loops/programmingProjects/06_Squares-To-n.c
@@ -0,0 +1,9 @@
+// Write a program that prompts the user to enter a number n, then prints all
+// even squares between 1 and n. For example, if the user enters 100, the
+// program should print the following:
+//
+// 4
+// 16
+// 36
+// 64
+// 100
