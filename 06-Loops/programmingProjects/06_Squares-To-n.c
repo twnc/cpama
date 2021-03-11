f9c99c562404daea2b9660ba761493778f69b385
twnc <17169285+twnc@users.noreply.github.com>
2021-03-11 12:41:11 +0000

    Ch06 Programming Project 6 completed

--- 06-Loops/programmingProjects/06_Squares-To-n.c
+++ 06-Loops/programmingProjects/06_Squares-To-n.c
@@ -7,3 +7,20 @@
 // 36
 // 64
 // 100
+
+#include<stdio.h>
+
+int main(void)
+{
+	int n;
+	printf("Please enter a positive integer: ");
+	scanf("%d", &n);
+
+	// Print all even squares between 1 and n
+	for(int i = 2; i * i < n; i += 2)
+		printf("%d\n", i * i);
+
+	printf("%d\n", n);
+
+	return 0;
+}
