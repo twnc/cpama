dfada9e6b71503d8cdeef476cc4bcf799ebee324
twnc <17169285+twnc@users.noreply.github.com>
2021-03-11 11:12:47 +0000

    Ch06 Programming Project 4 added

---
+++ 06-Loops/programmingProjects/04_Loop-Broker.c
@@ -0,0 +1,43 @@
+// Add a loop to the broker.c program of Section 5.2 so that the user can enter
+// more than one trade and the program will calculate the commission on each.
+// The program should terminate when the user enters 0 as the trade value:
+//
+// Enter value of trade: 30000
+// Commission: $166.00
+//
+// Enter value of trade: 20000
+// Commission: $144.00
+//
+// Enter value of trade: 0
+//
+// ORIGNAL: Calculates a broker's commission
+
+#include<stdio.h>
+
+int main(void)
+{
+	float commission, value;
+
+	printf("Enter value of trade: ");
+	scanf("%f", &value);
+
+	if(value < 2500.00f)
+		commission = 30.00f + value * .017f;
+	else if (value < 6250.00f)
+		commission = 56.00f + value * .0066f;
+	else if (value < 20000.00f)
+		commission = 76.00f + value * .0034f;
+	else if (value < 50000.00f)
+		commission = 100.00f + value * .0022f;
+	else if (value < 500000.00f)
+		commission = 155.00f + value * .0011f;
+	else
+		commission = 255.00f + value * .0009f;
+
+	if (commission < 39.00f)
+		commission = 39.00f;
+
+	printf("Commission: $%.2f\n", commission);
+
+	return 0;
+}
