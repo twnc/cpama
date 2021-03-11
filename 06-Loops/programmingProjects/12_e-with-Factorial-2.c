329e6e7545c03605a432738b0bc9ceecde84159e
twnc <17169285+twnc@users.noreply.github.com>
2021-03-11 20:05:27 +0000

    Ch06 Programming Project 12 added

---
+++ 06-Loops/programmingProjects/12_e-with-Factorial-2.c
@@ -0,0 +1,35 @@
+// Modify Programming Project 11 so that the program continues adding terms
+// until the current term becomes less than ɛ, where ɛ is a small
+// (floating-point) number entered by the user.
+// ORIGINAL:
+// The value of the constant of the mathematical constant e can be expressed as
+// an infinite series:
+// e = 1 + 1/1! + 1/2! + 1/3! + ...
+// Write a program that approximates e by computing the value of
+// 1 + 1/1! + 1/2! + 1/3! + ... 1/n!
+// where n is an integer entered by the user.
+
+#include<stdio.h>
+
+int main(void)
+{
+	float e = 1, factorial = 1;
+	int iterations;
+
+	printf("The value of the constant of the mathematical constant e can ");
+	printf("be expressed as an infinite series:\n");
+	printf("e = 1 + 1/1! + 1/2! + 1/3! + ...\n");
+	printf("We can aproximate the value of e by computing the value of \n");
+	printf("1 + 1/1! + 1/2! + 1/3! + ... 1/n!\n");
+	printf("What would you like n to be?\n");
+	scanf("%d", &iterations);
+
+	for(int i = 1; i <= iterations; i++) {
+		factorial *= i;
+		e += 1 / factorial;
+	}
+
+	printf("\ne = %f\n", e);
+
+	return 0;
+}
