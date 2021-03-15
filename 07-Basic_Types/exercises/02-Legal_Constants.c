3d3b0e9065c406a484267008809e965a38577433
twnc <17169285+twnc@users.noreply.github.com>
2021-03-15 15:58:40 +0000

    Ch07 Exercise 2 added

---
+++ 07-Basic_Types/exercises/02-Legal_Constants.c
@@ -0,0 +1,7 @@
+// Which of the following are not legal constants in C? Classify each legal
+// constant as either integer or floating-point.
+// (a) 010E2
+// (b) 32.1E+5
+// (c) 0790
+// (d) 100_000
+// (e) 3.978e-2
