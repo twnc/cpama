39715956bfb6571cb580ca3540647d45347d1533
twnc <17169285+twnc@users.noreply.github.com>
2021-03-16 11:28:48 +0000

    Ch07 Programming Project 3 completed

---
+++ 07-Basic_Types/programmingProjects/03-Double_Sum.c
@@ -0,0 +1,22 @@
+// Modify the sum2.c program of Section 7.1 to sum a series of double values.
+// ORIGINAL:
+// Sums a series of numbers (using long variables)
+
+#include<stdio.h>
+
+int main(void)
+{
+	double n, sum = 0;
+
+	printf("This program sums a series of integers.\n");
+	printf("Enter integers (0 to terminate): ");
+
+	scanf("%lf", &n);
+	while(n != 0) {
+		sum += n;
+		scanf("%lf", &n);
+	}
+	printf("The sum is: %g\n", sum);
+
+	return 0;
+}
