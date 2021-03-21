aa3c0000e5f30c951d147a18dfadbd7a138d74d0
twnc <17169285+twnc@users.noreply.github.com>
2021-03-21 15:57:38 +0000

    Ch07 Programming Project 2 completed

--- 07-Basic_Types/programmingProjects/02-Enter_to_continue.c
+++ 07-Basic_Types/programmingProjects/02-Enter_to_continue.c
@@ -17,9 +17,16 @@ int main(void)
 	printf("This program prints a table of squares.\n");
 	printf("Enter number of entries in table: ");
 	scanf("%d", &n);
+	getchar(); // Gets \n ignored by scanf to flush buffer
 
 	for(i = 1; i <= n; i++) {
 		printf("%10d%10d\n", i, i * i);
+		if(i % 24 == 0) {
+			printf("Press Enter to continue...");
+			// Reads characters until users presses enter
+			while(getchar() != '\n')
+				;
+		}
 	}
 
 	return 0;
