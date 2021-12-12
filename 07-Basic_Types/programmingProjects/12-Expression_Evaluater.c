18a85a23c5bfbd4785a33d2c146676344361e653
twnc <17169285+twnc@users.noreply.github.com>
2021-12-12 21:11:21 +0000

    Ch07 Programming Project 12 - Rewrote program to take longer equations

--- 07-Basic_Types/programmingProjects/12-Expression_Evaluater.c
+++ 07-Basic_Types/programmingProjects/12-Expression_Evaluater.c
@@ -1,63 +1,40 @@
 // Write a program that evaluates an expression:
 // Enter an expression: 1+2.5*3
 // Value of expression: 10.5
-// The operands in the expression are floating-point numbers; the operators are
+// The operands in the expression are floating-point numbers; ther operators are
 // +, -, *, and /. The expression is evaluated from left to right (no operator
 // takes precedence over any other operator).
-
 #include<stdio.h>
-#include<stdbool.h>
 
 int main(void)
 {
-	float val1, val2, val3, result;
-	char operator1, operator2;
-	bool valid_expression = true;
+	float value, new_number;
+	char operator;
 
 	printf("Enter an expression: ");
-	scanf("%f %c%f %c%f", &val1, &operator1, &val2, &operator2, &val3);
+	fflush(stdout);
+	
+	scanf("%f", &value);
 
-	result = val1;
-	switch(operator1) {
-		case '+':
-			result += val2;
-			break;
-		case '-':
-			result -= val2;
-			break;
-		case '*':
-			result *= val2;
-			break;
-		case '/':
-			result /= val2;
-			break;
-		default:
-			valid_expression = false;
-			break;
-	}
-	switch(operator2) {
-		case '+':
-			result += val3;
-			break;
-		case '-':
-			result -= val3;
-			break;
-		case '*':
-			result *= val3;
-			break;
-		case '/':
-			result /= val3;
-			break;
-		default:
-			valid_expression = false;
-			break;
+	// Find first character that isn't whitespace
+	while((operator = getchar()) == ' ')
+		;
+
+	// Apply operator to next number
+	while(operator != '\n') {
+		scanf("%f", &new_number);
+		switch (operator) {
+			case '+': value += new_number; break;
+			case '-': value -= new_number; break;
+			case '*': value *= new_number; break;
+			case '/': value /= new_number; break;
+			default: printf("ERROR! Invalid operator\n");
+		}
+		while((operator = getchar()) == ' ')
+			;
 	}
 
-
-	if(valid_expression)
-		printf("Value of expression: %g\n", result);
-	else
-		printf("Invalid expression.\n");
+	printf("Value of expression: %g\n", value);
 
 	return 0;
 }
