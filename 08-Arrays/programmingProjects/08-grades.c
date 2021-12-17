9c570d5006df59cd86eec9d88034ce8b96873296
twnc <17169285+twnc@users.noreply.github.com>
2021-12-17 12:29:44 +0000

    Ch08 Programming Project 8 completed

--- 08-Arrays/programmingProjects/08-grades.c
+++ 08-Arrays/programmingProjects/08-grades.c
@@ -16,39 +16,50 @@
 
 #include<stdio.h>
 
-#define ROWS 5
-#define COLUMNS 5
+#define STUDENTS 5
+#define QUIZ 5
 
 int main(void)
 {
-	int matrix[ROWS][COLUMNS];
+	int grades[STUDENTS][QUIZ];
 
 	// Collect user input
-	for (int i = 0; i < ROWS; ++i) {
-		printf("Enter row %d: ", i + 1);
+	for (int i = 0; i < STUDENTS; ++i) {
+		printf("Enter student %d's quiz scores: ", i + 1);
 		fflush(stdout);
-		for (int j = 0; j < COLUMNS; ++j)
-			scanf("%d", &matrix[i][j]);
+		for (int j = 0; j < QUIZ; ++j)
+			scanf("%d", &grades[i][j]);
 	}
 	printf("\n");
 
-	// Print row totals
-	printf("Row totals:");
-	for (int i = 0; i < ROWS; ++i) {
+	// Print each student's total score and average score
+	for (int i = 0; i < STUDENTS; ++i) {
 		int total = 0;
-		for (int j = 0; j < COLUMNS; ++j)
-			total += matrix[i][j];
-		printf(" %d", total);
+		for (int j = 0; j < QUIZ; ++j)
+			total += grades[i][j];
+		printf("Student %d's total score was   %3d\n", i + 1, total);
+		printf("Student %d's ", i + 1);
+		printf("average score was %3g\n\n", (double) total / QUIZ);
 	}
 	printf("\n");
 
-	// Print column totals
-	printf("Column totals:");
-	for (int i = 0; i < COLUMNS; ++i) {
+	// Print average score, low score and high score for each quiz
+	for (int i = 0; i < QUIZ; ++i) {
 		int total = 0;
-		for (int j = 0; j < ROWS; ++j)
-			total += matrix[j][i];
-		printf(" %d", total);
+		// Set high and low score to first grade
+		int low_score, high_score;
+		low_score = high_score = grades[0][i];
+		// Calculate total score, high and low score for current quiz
+		for (int j = 0; j < STUDENTS; ++j) {
+			if (grades[j][i] < low_score)
+				low_score = grades[j][i];
+			else if (grades[j][i] > high_score)
+				high_score = grades[j][i];
+			total += grades[j][i];
+		}
+		// Print results
+		printf("Quiz %d:\nAverage score:%4d, ", i + 1, total / STUDENTS);
+		printf("High score:%4d, Low score:%4d\n", high_score, low_score);
 	}
 	printf("\n");
 
