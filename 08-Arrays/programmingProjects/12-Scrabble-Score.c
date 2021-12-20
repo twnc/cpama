601538b4bd92c017494b7675ddd53f9ba25f0fb9
twnc <17169285+twnc@users.noreply.github.com>
2021-12-20 13:52:43 +0000

    Ch08 Programming Project 12 completed

--- 08-Arrays/programmingProjects/12-Scrabble-Score.c
+++ 08-Arrays/programmingProjects/12-Scrabble-Score.c
@@ -1,9 +1,9 @@
 // Modify Programming Project 5 from Chapter 7 so that the SCRABBLE values of
-// the letters are stored in an array. The array wil have 26 elements,
+// the letters are stored in an array. The array will have 26 elements,
 // corresponding to the 26 letters of the alphabet. For example, element 0 of
 // the array will store 1 (because the SCRABBLE value of the letter A is 1),
 // element 1 of the array will store 3 (because the SCRABBLE value of the letter
-// B is 3), and so forth. As each character of the inpiut word is read, the
+// B is 3), and so forth. As each character of the input word is read, the
 // program will use the array to determine the SCRABBLE value of the character.
 // Use an array initialiser to set up the array.
 //
@@ -33,37 +33,17 @@ int main(void)
 {
 	int score = 0;
 	char ch;
+	int letter_values[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3,
+		10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 	printf("Enter a word: ");
+	fflush(stdout);
 	while((ch = getchar()) != '\n') {
 		ch = toupper(ch);
-		switch (ch) {
-			case 'A': case 'E': case 'I': case 'L': case 'N':
-			case 'O': case 'R': case 'S': case 'T': case 'U':
-				score += 1;
-				break;
-			case 'D': case 'G':
-				score += 2;
-				break;
-			case 'B': case 'C': case 'M': case 'P':
-				score += 3;
-				break;
-			case 'F': case 'H': case 'V': case 'W': case 'Y':
-				score += 4;
-				break;
-			case 'K':
-				score += 5;
-				break;
-			case 'J': case 'X':
-				score += 8;
-				break;
-			case 'Q': case 'Z':
-				score += 10;
-				break;
-			default:
-				printf("Character '%c' not recognised\n", ch);
-				break;
-		}
+		if (ch >= 'A' && ch <= 'Z')
+			score += letter_values[ch - 'A'];
+		else
+			printf("Character '%c' not recognised\n", ch);
 	}
 
 	printf("Scrabble value: %d\n", score);
