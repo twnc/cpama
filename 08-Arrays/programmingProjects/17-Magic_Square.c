367b846fb2162926a87ea6e5997541aecb2f07b5
twnc <17169285+twnc@users.noreply.github.com>
2021-12-27 18:53:46 +0000

    Ch08 Programming Project 17 completed

--- 08-Arrays/programmingProjects/17-Magic_Square.c
+++ 08-Arrays/programmingProjects/17-Magic_Square.c
@@ -21,3 +21,91 @@
 // already occupied, put the number directly below the previously stored number.
 // If your compiler supports variable-length arrays, declare the array to have n
 // rows and n columns. If not, declare the array to have 99 rows and 99 columns.
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(void)
+{
+	int n;
+
+	printf("This program creates a magic square of a specified size.\n");
+	printf("The size must be an odd number between 1 and 99\n");
+	printf("Enter size of magic square: ");
+	fflush(stdout);
+	scanf("%d", &n);
+
+	int magic_square[n][n];
+	// Initialise array to zero
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			magic_square[i][j] = 0;
+
+	int x = 0, y = (n / 2);
+	for (int i = 1; i <= (n * n); ++i) {
+		magic_square[x][y] = i;
+		x = (x + n - 1) % n;
+		y = (y + 1) % n;
+		if (magic_square[x][y] != 0) {
+			x = (x + 2) % n;
+			y = (y + n - 1) % n;
+		}
+	}
+
+	// Print results
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j)
+			printf("%5d", magic_square[i][j]);
+		printf("\n");
+	}
+
+	// Check that it is a valid magic square
+	bool valid = true;
+	int sum_of_first_row = 0;
+	int sum;
+	for (int i = 0; i < n; ++i)
+		sum_of_first_row += magic_square[0][i];
+
+	// Check rows
+	for (int i = 1; i < n; ++i) {
+		sum = 0;
+		for (int j = 0; j < n; ++j)
+			sum += magic_square[i][j];
+		if (sum != sum_of_first_row) {
+			valid = false;
+			break;
+		}
+	}
+	// Check columns
+	for (int j = 0; j < n; ++j) {
+		sum = 0;
+		for (int i = 0; i < n; ++i)
+			sum += magic_square[i][j];
+		if (sum != sum_of_first_row) {
+			valid = false;
+			break;
+		}
+	}
+	// Check diagonals
+	sum = 0;
+	for (int i = 0; i < n; ++i) {
+		sum += magic_square[i][i];
+	}
+	if (sum != sum_of_first_row)
+		valid = false;
+	int j = n - 1;
+	sum = 0;
+	for(int i = 0; i < n; ++i) {
+		sum += magic_square[i][j];
+		--j;
+	}
+	if (sum != sum_of_first_row)
+		valid = false;
+
+	if (valid)
+		printf("This is a valid magic square.\n");
+	else
+		printf("This is NOT a valid magic square.\n");
+
+	return 0;
+}
