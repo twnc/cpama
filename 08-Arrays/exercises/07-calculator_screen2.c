e64a9288d351e5387ee0f8f5608daf445394e8cf
twnc <17169285+twnc@users.noreply.github.com>
2021-12-31 19:39:37 -0500

    Ch08 Exercise 7 added

---
+++ 08-Arrays/exercises/07-calculator_screen2.c
@@ -0,0 +1,63 @@
+// Using the shortcuts described in Section 8.2, shrink the initializer for the
+// segments array (Exercise 6) as much as you can.
+// ORIGINAL:
+// Calculators, watches, and other electronic devies often rely on seven-segment
+// displays for numerical output. To form a digit, such devices "turn on" some
+// of the seven segments while leaving others "off":
+//  _       _   _       _   _   _   _   _
+// | |   |  _|  _| |_| |_  |_    | |_| |_|
+// |_|   | |_   _|   |  _| |_|   | |_|  _|
+// Suppose that we want to set up an array that remembers which segments should
+// be "on" for each digit. Let's number the segments as follows:
+// Hers' what the array might look like, with each row representing one digit:
+// const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, ...};
+// I've given you the first row of the initializer, fill in the rest.
+
+#include<stdio.h>
+
+int main(void)
+{
+	const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0},
+				     {0, 1, 1, 0, 0, 0, 0},
+	                             {1, 1, 0, 1, 1, 0, 1},
+	                             {1, 1, 1, 1, 0, 0, 1},
+	                             {0, 1, 1, 0, 0, 1, 1},
+	                             {1, 0, 1, 1, 0, 1, 1},
+	                             {1, 0, 1, 1, 1, 1, 1},
+	                             {1, 1, 1, 0, 0, 0, 0},
+	                             {1, 1, 1, 1, 1, 1, 1},
+	                             {1, 1, 1, 1, 0, 1, 1}};
+
+	for (int i = 0; i < 10; ++i) {
+		// First row
+		putchar(' ');
+		if (segments[i][0])
+			putchar('_');
+		else putchar(' ');
+		putchar(' ');
+		putchar('\n');
+		// Second row
+		if (segments[i][5])
+			putchar('|');
+		else putchar(' ');
+		if (segments[i][6])
+			putchar('_');
+		else putchar(' ');
+		if (segments[i][1])
+			putchar('|');
+		else putchar(' ');
+		putchar('\n');
+		// Third row
+		if (segments[i][4])
+			putchar('|');
+		else putchar(' ');
+		if (segments[i][3])
+			putchar('_');
+		else putchar(' ');
+		if (segments[i][2])
+			putchar('|');
+		else putchar(' ');
+		putchar('\n');
+	}
+	return 0;
+}
