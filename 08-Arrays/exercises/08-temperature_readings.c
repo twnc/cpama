3cc1af2935e709db624a40dbb11a8d44ba30acc6
twnc <17169285+twnc@users.noreply.github.com>
2022-01-06 22:32:53 -0500

    Ch08 Exercise 8 completed

---
+++ 08-Arrays/exercises/08-temperature_readings.c
@@ -0,0 +1,11 @@
+// Write a declaration for a two-dimensional array named temperature_readings
+// that stores one month of hourly temperature readings. (For simplicity, assume
+// that a month has 30 days.) The rows of the array should represent days of the
+// month; the column should represent hours of the day.
+
+int main(void)
+{
+	double temperature_readings[30][24] = {0};
+
+	return 0;
+}
