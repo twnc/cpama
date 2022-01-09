770557460a7645777998c06130f24613928be4f0
twnc <17169285+twnc@users.noreply.github.com>
2022-01-08 23:05:49 -0500

    Ch09 Exercise 3 completed

---
+++ 09-Functions/exercises/03-euclid.c
@@ -0,0 +1,39 @@
+// Write a function gcd(m, n) that calculates the greatest common divisor of the
+// integers m and n. (Programming Project 2 in Chapter 6 describes Euclid's
+// algorithm for computing the GCD.)
+
+#include<stdio.h>
+
+int gcd(int m, int n)
+{
+	int biggest, smallest, common_divisor;
+	if (m > n) {
+		biggest = m;
+		smallest = n;
+	} else {
+		biggest = n;
+		smallest = m;
+	}
+	common_divisor = smallest;
+
+	while ((biggest % smallest) != 0) {
+		common_divisor = biggest % smallest;
+		biggest = smallest;
+		smallest = common_divisor;
+	}
+
+	return common_divisor;
+}
+
+int main(void)
+{
+	int n1, n2;
+
+	printf("Please enter two integers: ");
+	fflush(stdout);
+	scanf("%d%d", &n1, &n2);
+
+	int result = gcd(n1, n2);
+	printf("The greatest common divisor of %d and %d is %d.\n", n1, n2, result);
+	return 0;
+}
