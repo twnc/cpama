48ecfa7f20c985993381c78489f4c4d44af59a90
twnc <17169285+twnc@users.noreply.github.com>
2022-01-11 15:54:12 -0500

    Ch09 Exercise 7 completed

--- 09-Functions/exercises/07-legal_statements.c
+++ 09-Functions/exercises/07-legal_statements.c
@@ -7,8 +7,35 @@
 // (b) x = f(83, 12);
 // ANSWER: Legal, you can save an int into a double
 // (c) i = f(3.15, 9.28);
-// ANSWER: Not sure if you implicit casting will work for function parameters
+// ANSWER: Legal, function parameters will have narrowing conversion to 3 and 9
 // (d) x = f(3.15, 9.28);
-// ANSWER: Same as above
+// ANSWER: Legal, function parameters will have narrowing conversion to 3 and 9
 // (e) f(83,12);
 // ANSWER: Legal, you can call a function witout using it's output
+// Answers checked by compiling the code below!
+
+#include<stdio.h>
+
+int f(int a, int b)
+{
+	return 1;
+}
+
+int main(void)
+{
+	int i;
+	double x;
+
+	// (a)
+	i = f(83, 12);
+	// (b)
+	x = f(83, 12);
+	// (c)
+	i = f(3.15, 9.28);
+	// (d)
+	x = f(3.15, 9.28);
+	// (e)
+	f(83,12);
+
+	return 0;
+}
