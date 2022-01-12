de7734a68d4f7b9fe8c321bd89f04905f8adb67e
twnc <17169285+twnc@users.noreply.github.com>
2022-01-11 23:50:41 -0500

    Ch09 Exercise 9 solution tested

--- 09-Functions/exercises/09-swap.c
+++ 09-Functions/exercises/09-swap.c
@@ -2,3 +2,22 @@
 // ANSWER: i = 1, j = 2
 // This will be because the function only modifies copies of the values and
 // doesn't return them.
+#include<stdio.h>
+
+void swap (int a, int b);
+
+int main(void)
+{
+	int i = 1, j = 2;
+
+	swap(i, j);
+	printf("i = %d, j = %d\n", i, j);
+	return 0;
+}
+
+void swap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
