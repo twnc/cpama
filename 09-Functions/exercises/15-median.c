033e8728a33ad666b91566f025dce3cd46f035be
twnc <17169285+twnc@users.noreply.github.com>
2022-01-13 18:49:32 -0500

    Ch09 Exercise 15 completed

--- 09-Functions/exercises/15-median.c
+++ 09-Functions/exercises/15-median.c
@@ -1,13 +1,33 @@
 // The following (rather confusing) function finds the median of three numbers.
 // Rewrite the function so that it has just one return statement.
+#include<stdio.h>
 
 double median(double x, double y, double z)
 {
-	if (x <= y)
-		if (y <= z) return y;
-		else if (x <= z) return z;
-		else return x;
-	if (z <= y) return y;
-	if (x <= z) return x;
-	return z;
+	double result = z;
+	if (x <= y) {
+		if (y <= z)
+			result = y;
+		else if (x <= z)
+			result = z;
+		else
+			result = x;
+	}
+	else if (z <= y)
+		result = y;
+	else if (x <= z)
+		result = x;
+
+	return result;
+}
+
+int main(void)
+{
+	double x = 10;
+	double y = 14;
+	double z = 12;
+
+	printf("%g %g %g\nMedian: %g\n", x, y, z, median(x, y, z));
+
+	return 0;
 }
