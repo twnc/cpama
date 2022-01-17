e456f4cb72d6c75089d1d31061e7d7eb6b04bf6c
twnc <17169285+twnc@users.noreply.github.com>
2022-01-17 22:07:50 +0000

    Ch09 Exercise 17 completed

--- 09-Functions/exercises/17-nonrecursive_fact.c
+++ 09-Functions/exercises/17-nonrecursive_fact.c
@@ -17,8 +17,9 @@ int main(void)
 
 int fact(int n)
 {
-	if (n <= 1)
-		return 1;
-	else
-		return n * fact(n - 1);
+	int result = 1;
+	for (int i = 2; i <= n; i++)
+		result *= i;
+
+	return result;
 }
